278121fb56cafa2b958468d7eca4cfe1cf203292
Diogo Sales <diogodobu@gmail.com>
2024-10-14 13:45:27 -0300

    correcao animacao do texto menu

--- js/mobileNavbar.js
+++ js/mobileNavbar.js
@@ -8,9 +8,15 @@ class MobileNavbar {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  animatedLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+    });
+  }
+
   handleClick() {
-    console.log(this);
     this.navList.classList.toggle(this.activeClass);
+    this.animatedLinks();
   }
 
   addClickEvent() {
