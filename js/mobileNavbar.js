7da11d1b8edc6b932db47589700a83eb1aab598b
Diogo Sales <diogodobu@gmail.com>
2024-10-14 13:23:53 -0300

    ajustado estilo e criado js

---
+++ js/mobileNavbar.js
@@ -0,0 +1,22 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", () => console.log("Funcionando"));
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(".mobileMenu", ".menu", ".menu li");
+mobileNavbar.init();
